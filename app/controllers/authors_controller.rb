a7cdcdc27e79813febb86649363d3419914d40f8
otieno-derrick <otieno.derrrick@student.moringaschool.com>
2022-10-01 11:16:13 +0300

    done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,13 +2,13 @@ class AuthorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    authors = Author.all 
-    render json: authors
+    authors = Author.all
+    render json: authors, include: ['tags','posts.tags','profile','author.profile']
   end
 
   def show
     author = Author.find(params[:id])
-    render json: author
+    render json: author, include: ['tags','posts.tags','profile','author.profile']
   end
 
   private
